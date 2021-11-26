2bfca86ab2a80100e74b4313615834b21db9a60e
Aditya Pratama Badra <orochimadit@gmail.com>
2021-11-26 11:01:39 +0700

    fix scoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { ScoringsComponent } from './scorings/scorings.component';
 import { AdminComponent } from './admin/admin.component';
 import { StandingsComponent } from './standings/standings.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScoringsComponent,
     AdminComponent,
-    StandingsComponent
+    StandingsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
